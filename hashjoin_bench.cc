acc0fe9c604b93faf3ec73cf85a1e671d2bdc9a1
Felix Chern <idryman@gmail.com>
2018-01-28 17:51:57 -0800

    why sorted order has no perf difference on table lookup?

--- hashjoin_bench.cc
+++ hashjoin_bench.cc
@@ -1,5 +1,7 @@
 #include <benchmark/benchmark.h>
 #include <string>
+#include <unordered_map>
+#include "radix_hash.h"
 #include "hashjoin.h"
 #include "strgen.h"
 
@@ -39,8 +41,46 @@ static void BM_radix_hash_join(benchmark::State& state) {
   }
 }
 
-BENCHMARK(BM_single_table_join)->Range(1<<10, 1<<18)->Complexity();
-BENCHMARK(BM_radix_hash_table_join)->Range(1<<10, 1<<18)->Complexity();
-BENCHMARK(BM_radix_hash_join)->Range(1<<10, 1<<18)->Complexity();
+static void BM_hash_join_raw(benchmark::State& state) {
+  int size = state.range(0);
+  auto r = ::create_strvec(size);
+  auto s = ::create_strvec(size);
+  std::unordered_map<std::string, uint64_t> s_map;
+  s_map.reserve(s.size());
+  for (auto s_kv : s) {
+    s_map[s_kv.first] = s_kv.second;
+  }
+  for (auto _ : state) {
+    for (auto r_kv : r) {
+      benchmark::DoNotOptimize(s_map[r_kv.first]);
+    }
+  }
+}
+
+static void BM_radix_hash_join_raw(benchmark::State& state) {
+  int size = state.range(0);
+  auto r = ::create_strvec(size);
+  auto s = ::create_strvec(size);
+  std::unordered_map<std::string, uint64_t> s_map;
+  s_map.reserve(s.size());
+  for (auto s_kv : s) {
+    s_map[s_kv.first] = s_kv.second;
+  }
+  auto r_sorted = KeyValVec(r.size());
+  ::radix_hash_df1<std::string, uint64_t>(r.begin(), r.end(), r_sorted.begin(),
+                                          r.size(), 11, 0);
+
+  for (auto _ : state) {
+    for (auto r_kv : r_sorted) {
+      benchmark::DoNotOptimize(s_map[r_kv.first]);
+    }
+  }
+}
+
+//BENCHMARK(BM_single_table_join)->Range(1<<10, 1<<18)->Complexity();
+//BENCHMARK(BM_radix_hash_table_join)->Range(1<<10, 1<<18)->Complexity();
+//BENCHMARK(BM_radix_hash_join)->Range(1<<10, 1<<18)->Complexity();
+BENCHMARK(BM_hash_join_raw)->Range(1<<10, 1<<18)->Complexity();
+BENCHMARK(BM_radix_hash_join_raw)->Range(1<<10, 1<<18)->Complexity();
 
 BENCHMARK_MAIN();
