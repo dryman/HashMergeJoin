f6a44f54ae60030b16294c45997f0cdf2d205f3e
dryman <idryman@gmail.com>
2018-02-28 07:39:18 -0800

    scratch inplace counting sort

--- hashjoin.h
+++ hashjoin.h
@@ -91,8 +91,11 @@ class HashMergeJoin {
                                  mask_bits, 11, 0);
     ::radix_hash_bf1<Key, SValue>(s_begin, s_end, s_sorted.begin(),
                                  mask_bits, 11, 0);
-    mask = (1ULL << mask_bits) - 1;
-    HashMergeJoin(mask, r_sorted, s_sorted);
+    std::cout << "rs size before whole setup: " << r_sorted.size() << "\n";
+    _mask = (1ULL << mask_bits) - 1;
+    _r_sorted = r_sorted;
+    _s_sorted = s_sorted;
+    //HashMergeJoin(mask, r_sorted, s_sorted);
   }
 
   class iterator : std::iterator<std::input_iterator_tag,
@@ -104,6 +107,8 @@ class HashMergeJoin {
     _mask(mask), _rs_iter(rs_iter), _rs_end(rs_end),
       _ss_iter(ss_iter), _ss_end(ss_end) {
       std::size_t r_hash, s_hash;
+      //std::cout << "rs distance: " << std::distance(_rs_iter, rs_end) << "\n";
+      //std::cout << _rs_iter << _rs_end;
       while (_rs_iter != _rs_end) {
         if (_ss_iter == _ss_end) {
           _rs_iter = _rs_end;
@@ -188,7 +193,11 @@ class HashMergeJoin {
       }
       ++_rs_iter;
       ++_ss_iter;
-      while (_rs_iter != _rs_end) {
+      while (true) {
+        if (_rs_iter == _rs_end) {
+          _ss_iter = _ss_end;
+          break;
+        }
         if (_ss_iter == _ss_end) {
           _rs_iter = _rs_end;
           break;
@@ -233,11 +242,11 @@ class HashMergeJoin {
     }
     bool operator==(iterator other) const {
       return _rs_iter == other._rs_iter &&
-      _ss_iter == other._ss_iter &&
-      _ss_dupcnt == other._ss_dupcnt;
+      _ss_iter == other._ss_iter;
+      //_ss_dupcnt == other._ss_dupcnt;
     }
     bool operator!=(iterator other) const {
-      return !(*this == other);
+      return _rs_iter != other._rs_iter || _ss_iter != other._ss_iter;
     }
     std::tuple<Key*, RValue*, SValue*>& operator*() {
       tmp_val = std::make_tuple(&std::get<1>(*_rs_iter),
@@ -257,9 +266,10 @@ class HashMergeJoin {
 
  public:
   iterator begin() {
+    std::cout << "rs size: " << _r_sorted.size() << "\n";
     return iterator(_mask,
-                    _r_sorted.begin(), _r_sorted.end(),
-                    _s_sorted.begin(), _s_sorted.end());
+                    _r_sorted.end(), _r_sorted.end(),
+                    _s_sorted.end(), _s_sorted.end());
   }
 
   iterator end() {
@@ -315,10 +325,13 @@ template<typename RIter, typename SIter>
                          std::hash<Key>, SIter, SSortedIter>,
                          s_begin, s_end, s_sorted.begin(),
                          mask_bits, 11, 0);
-    mask = (1ULL << mask_bits) - 1;
+    //mask = (1ULL << mask_bits) - 1;
     r_thread.join();
     s_thread.join();
-    HashMergeJoin<RIter,SIter>(mask, r_sorted, s_sorted);
+    //HashMergeJoin<RIter,SIter>(mask, r_sorted, s_sorted);
+    HashMergeJoin<RIter,SIter>::_mask = (1ULL << mask_bits) - 1;
+    HashMergeJoin<RIter,SIter>::_r_sorted = r_sorted;
+    HashMergeJoin<RIter,SIter>::_s_sorted = s_sorted;
   }
 };
 
--- radix_hash.h
+++ radix_hash.h
@@ -6,6 +6,7 @@
 #include <utility>
 #include <vector>
 #include <functional>
+#include <sys/mman.h>
 
 // namespace radix_hash?
 
@@ -390,6 +391,17 @@ template <typename Key,
   buffers[1] = std::make_unique<
   std::tuple<std::size_t, Key, Value>[]>(input_num);
 
+  /*
+  madvise(&*begin, input_num * (sizeof(begin->first) + sizeof(begin->second)),
+          MADV_SEQUENTIAL);
+  madvise(&buffers[0], input_num *
+          (sizeof(begin->first) + sizeof(begin->second) + sizeof(std::size_t)),
+          MADV_SEQUENTIAL);
+  madvise(&buffers[1], input_num *
+          (sizeof(begin->first) + sizeof(begin->second) + sizeof(std::size_t)),
+          MADV_SEQUENTIAL);
+  */
+
   for (auto iter = begin; iter != end; ++iter) {
     h = Hash{}(std::get<0>(*iter));
     counters[(h & mask) >> shift]++;
@@ -407,8 +419,8 @@ template <typename Key,
       h = Hash{}(std::get<0>(*iter));
       dst_idx = counters[(h & mask) >> shift]++;
       std::get<0>(dst[dst_idx]) = h;
-      std::get<1>(dst[dst_idx]) = iter->first;
-      std::get<2>(dst[dst_idx]) = iter->second;
+      std::get<1>(dst[dst_idx]) = std::move(iter->first);
+      std::get<2>(dst[dst_idx]) = std::move(iter->second);
     }
     return;
   } else {
@@ -416,10 +428,13 @@ template <typename Key,
       h = Hash{}(std::get<0>(*iter));
       dst_idx = counters[(h & mask) >> shift]++;
       std::get<0>(buffers[0][dst_idx]) = h;
-      std::get<1>(buffers[0][dst_idx]) = iter->first;
-      std::get<2>(buffers[0][dst_idx]) = iter->second;
+      std::get<1>(buffers[0][dst_idx]) = std::move(iter->first);
+      std::get<2>(buffers[0][dst_idx]) = std::move(iter->second);
     }
   }
+
+  madvise(&*begin, input_num * (sizeof(begin->first) + sizeof(begin->second)),
+          MADV_DONTNEED);
 
   for (iter = 0; iter < num_iter - 2; iter++) {
     mask1 = (1ULL << (mask_bits - partition_bits * iter)) - 1;
@@ -449,7 +464,7 @@ template <typename Key,
         for (int k = anchor_idx; k < j; k++) {
           h = std::get<0>(buffers[iter & 1][k]);
           buffers[(iter + 1) & 1][idx_counters[(h & mask2) >> shift2]++] =
-          buffers[iter & 1][k];
+            std::move(buffers[iter & 1][k]);
         }
         // clear up counters and reset anchor
         for (int k = 0; k < partitions; k++) {
@@ -468,9 +483,13 @@ template <typename Key,
     for (int k = anchor_idx; k < input_num; k++) {
       h = std::get<0>(buffers[iter & 1][k]);
       buffers[(iter + 1) & 1][idx_counters[(h & mask2) >> shift2]++] =
-        buffers[iter & 1][k];
+      std::move(buffers[iter & 1][k]);
     }
   }
+
+  madvise(&*dst, input_num *
+          (sizeof(begin->first) + sizeof(begin->second) + sizeof(std::size_t)),
+          MADV_WILLNEED);
 
   // flush the last buffer to dst.
   mask1 = (1ULL << (mask_bits - partition_bits * iter)) - 1;
@@ -506,7 +525,7 @@ template <typename Key,
         h_cnt = (h & mask2) >> shift2;
         dst_idx = idx_counters[h_cnt]++;
         flush_counters[h_cnt]++;
-        *(dst + dst_idx) = buffers[iter & 1][k];
+        *(dst + dst_idx) = std::move(buffers[iter & 1][k]);
         if (nosort_bits == 0) {
           for (int m = flush_counters[h_cnt]; m < counters[h_cnt]; m++) {
             if (HashTupleCmp(*(dst + dst_idx), *(dst + dst_idx - 1), mask)) {
@@ -539,7 +558,7 @@ template <typename Key,
     h_cnt = (h & mask2) >> shift2;
     dst_idx = idx_counters[h_cnt]++;
     flush_counters[h_cnt]++;
-    *(dst + dst_idx) = buffers[iter & 1][k];
+    *(dst + dst_idx) = std::move(buffers[iter & 1][k]);
     if (nosort_bits == 0) {
       for (int m = flush_counters[h_cnt]; m < counters[h_cnt]; m++) {
         if (HashTupleCmp(*(dst + dst_idx), *(dst + dst_idx - 1), mask)) {
@@ -549,6 +568,153 @@ template <typename Key,
       }
     }
   }
+}
+
+struct RadixCounter {
+  enum Status {
+    ToFill,
+    Normal,
+    End,
+  };
+  int idx = 0;
+  int end = 0;
+  Status status = Normal;
+};
+
+template <typename Key,
+  typename Value,
+  typename Hash = std::hash<Key>,
+  typename BidirectionalIterator,
+  typename RandomAccessIterator>
+  void radix_hash_bf2(BidirectionalIterator begin,
+      BidirectionalIterator end,
+      RandomAccessIterator dst,
+      int mask_bits,
+      int partition_bits,
+      int nosort_bits) {
+  int input_num, num_iter, iter, shift, shift1, shift2, dst_idx, anchor_idx;
+  int partitions = 1 << partition_bits;
+  std::size_t h, h1, h2, h_cnt, mask, mask1, mask2, anchor_h1;
+
+  input_num = std::distance(begin, end);
+
+  if (mask_bits <= nosort_bits) {
+    for (auto iter = begin; iter != end; iter++) {
+      h = Hash{}(std::get<0>(*iter));
+      std::get<0>(*dst) = h;
+      std::get<1>(*dst) = iter->first;
+      std::get<2>(*dst) = iter->second;
+      ++dst;
+    }
+    return;
+  }
+
+  num_iter = (mask_bits - nosort_bits
+              + partition_bits - 1) / partition_bits;
+  mask = (1ULL << mask_bits) - 1;
+  shift = mask_bits - partition_bits;
+  shift = shift < 0 ? 0 : shift;
+
+  // invariant: counters[partitions] is the total count
+  int counters[partitions];
+  int flush_counters[partitions];
+  int idx_counters[partitions];
+  std::vector<RadixCounter> radix_counter(partitions);
+
+  for (int i = 0; i < partitions + 1; i++)
+    counters[i] = 0;
+
+  for (auto iter = begin; iter != end; ++iter) {
+    h = Hash{}(std::get<0>(*iter));
+    counters[(h & mask) >> shift]++;
+  }
+  for (int i = 1; i < partitions; i++) {
+    counters[i] += counters[i - 1];
+  }
+  for (int i = partitions - 1; i != 0; i--) {
+    counters[i] = counters[i - 1];
+  }
+  counters[0] = 0;
+
+  for (auto iter = begin; iter != end; ++iter) {
+    h = Hash{}(std::get<0>(*iter));
+    dst_idx = counters[(h & mask) >> shift]++;
+    std::get<0>(dst[dst_idx]) = h;
+    std::get<1>(dst[dst_idx]) = std::move(iter->first);
+    std::get<2>(dst[dst_idx]) = std::move(iter->second);
+  }
+
+  for (iter = 0; iter < num_iter - 1; iter++) {
+    mask1 = (1ULL << (mask_bits - partition_bits * iter)) - 1;
+    shift1 = mask_bits - partition_bits * (iter + 1);
+    shift1 = shift1 < 0 ? 0 : shift1;
+    mask2 = (1ULL << (mask_bits - partition_bits * (iter + 1))) - 1;
+    shift2 = mask_bits - partition_bits * (iter + 2);
+    shift2 = shift2 < 0 ? 0 : shift2;
+    for (int k = 0; k < partitions; k++) {
+      counters[k] = 0;
+    }
+    anchor_h1 = (std::get<0>(dst[0]) & mask1) >> shift1;
+    anchor_idx = 0;
+
+    for (int j = 0; j < input_num; j++) {
+      h = std::get<0>(dst[j]);
+      h1 = (h & mask1) >> shift1;
+      h2 = (h & mask2) >> shift2;
+      if (h1 == anchor_h1) {
+        counters[h2]++;
+      } else {
+        // flush counters
+        for (int k = 0; k < partitions; k++) {
+          flush_counters[k] = 0;
+        }
+        radix_counter[0] = {anchor_idx, anchor_idx + counters[0],
+                            RadixCounter::Status::Normal};
+        for (unsigned int k = 1; k < partitions; k++) {
+          int prev_end = radix_counter[k - 1].end;
+          radix_counter[k] = {prev_end, prev_end + counters[k],
+                              RadixCounter::Status::Normal};
+        }
+        int ctr = 0;
+        std::tuple<std::size_t, Key, Value> tmp;
+        while (ctr < partitions) {
+          if (radix_counter[ctr].idx == radix_counter[ctr].end) {
+            ctr++;
+            continue;
+          }
+
+        }
+        
+        /*
+        for (int k = anchor_idx; k < j; k++) {
+          h = std::get<0>(buffers[iter & 1][k]);
+          buffers[(iter + 1) & 1][idx_counters[(h & mask2) >> shift2]++] =
+            std::move(buffers[iter & 1][k]);
+        }
+        */
+        // clear up counters and reset anchor
+        for (int k = 0; k < partitions; k++) {
+          counters[k] = 0;
+        }
+        anchor_idx = j;
+        anchor_h1 = h1;
+        counters[h2]++;
+      }
+    }
+    // flush counters
+    /*
+    idx_counters[0] = anchor_idx;
+    for (unsigned int k = 1; k < partitions; k++) {
+      idx_counters[k] = idx_counters[k-1] + counters[k-1];
+    }
+    for (int k = anchor_idx; k < input_num; k++) {
+      h = std::get<0>(buffers[iter & 1][k]);
+      buffers[(iter + 1) & 1][idx_counters[(h & mask2) >> shift2]++] =
+      std::move(buffers[iter & 1][k]);
+    }
+    */
+  }
+
 }
 
 #endif
