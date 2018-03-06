583d64d6dd4e0e40934efcae26a1cb77e137e96c
Felix Chern <idryman@gmail.com>
2018-03-06 02:19:21 -0500

    Some minor changes to benchmark

--- radix_hash_bench.cc
+++ radix_hash_bench.cc
@@ -235,7 +235,7 @@ static void BM_radix_hash_bf4_str(benchmark::State& state) {
     ::radix_hash_bf4<std::string,uint64_t>(src.begin(),
                                            src.end(), dst.begin(),
                                            ::compute_power(size),
-                                           state.range(1), 0, 4);
+                                           state.range(1), 0, 32);
   }
   state.SetComplexityN(state.range(0));
 }
@@ -243,7 +243,7 @@ static void BM_radix_hash_bf4_str(benchmark::State& state) {
 
 static void RadixArguments(benchmark::internal::Benchmark* b) {
   for (int i = 11; i < 12; i+=1) {
-    for (int j = 1024; j <= (1UL << 20); j <<= 1) {
+    for (int j = (1 << 18); j <= (1UL << 23); j <<= 1) {
       b->Args({j, i});
     }
   }
@@ -261,15 +261,15 @@ static void RadixArguments(benchmark::internal::Benchmark* b) {
 // BENCHMARK(BM_radix_hash_bf2)->Apply(RadixArguments)->Complexity();
 //BENCHMARK(BM_radix_hash_bf3)->Apply(RadixArguments)->Complexity();
 //
-BENCHMARK(BM_radix_hash_df1_str)->Apply(RadixArguments)
-->Complexity(benchmark::oN);
+//BENCHMARK(BM_radix_hash_df1_str)->Apply(RadixArguments)
+//->Complexity(benchmark::oN);
 //BENCHMARK(BM_radix_hash_df2_str)->Apply(RadixArguments)->Complexity();
 //BENCHMARK(BM_radix_hash_bf1_str)->Apply(RadixArguments)->Complexity();
 //BENCHMARK(BM_radix_hash_bf2_str)->Apply(RadixArguments)->Complexity();
-BENCHMARK(BM_radix_hash_bf3_str)->Apply(RadixArguments)
-->Complexity(benchmark::oN);
-BENCHMARK(BM_qsort_string)->Apply(RadixArguments)
-->Complexity(benchmark::oN);
+//BENCHMARK(BM_radix_hash_bf3_str)->Apply(RadixArguments)
+//->Complexity(benchmark::oN);
+//BENCHMARK(BM_qsort_string)->Apply(RadixArguments)
+//->Complexity(benchmark::oN);
 BENCHMARK(BM_radix_hash_bf4_str)->Apply(RadixArguments)
 ->Complexity(benchmark::oN);
 
