27efe8a94a4659e72943417e3f8435cbca70aca8
Felix Chern <idryman@gmail.com>
2018-03-12 01:49:29 -0400

    Add array based HashMergeJoin impl, pref is similar

--- hashjoin.h
+++ hashjoin.h
@@ -187,4 +187,193 @@ class HashMergeJoin {
   std::vector<std::tuple<std::size_t, Key, SValue>> _s_sorted;
 };
 
+template<typename RIter, typename SIter>
+class HashMergeJoin2 {
+  static_assert(std::is_same<
+                typename RIter::value_type::first_type,
+                typename SIter::value_type::first_type>::value,
+                "RIter and SIter key type must be the same");
+  static_assert(std::is_same<
+                typename RIter::difference_type,
+                typename SIter::difference_type>::value,
+                "RIter and SIter difference type must be the same");
+
+  typedef typename RIter::difference_type distance_type;
+  typedef typename RIter::value_type::first_type Key;
+  typedef typename RIter::value_type::second_type RValue;
+  typedef typename SIter::value_type::second_type SValue;
+  typedef typename std::tuple<std::size_t, Key, RValue> RTuple;
+  typedef typename std::tuple<std::size_t, Key, SValue> STuple;
+  typedef typename std::vector<RTuple>::iterator RSortedIter;
+  typedef typename std::vector<STuple>::iterator SSortedIter;
+
+  //protected:
+ public:
+  HashMergeJoin2(RIter r_begin, RIter r_end,
+                 SIter s_begin, SIter s_end) {
+    distance_type r_size, s_size;
+    unsigned int cores = std::thread::hardware_concurrency();
+    _r_size = std::distance(r_begin, r_end);
+    _s_size = std::distance(s_begin, s_end);
+
+    _r_sorted = new std::tuple<std::size_t, Key, RValue>[_r_size];
+    _s_sorted = new std::tuple<std::size_t, Key, SValue>[_s_size];
+
+    ::radix_hash_bf6<Key, RValue>(r_begin, r_end, _r_sorted,
+                                  11, 0, cores);
+
+    ::radix_hash_bf6<Key, SValue>(s_begin, s_end, _s_sorted,
+                                  11, 0, cores);
+  }
+  ~HashMergeJoin2() {
+    delete[] _r_sorted;
+    delete[] _s_sorted;
+  }
+
+  class iterator : std::iterator<std::input_iterator_tag,
+    std::tuple<Key*, RValue*, SValue*>> {
+  public:
+  iterator(
+    std::tuple<std::size_t, Key, RValue>* r_arr,
+    std::tuple<std::size_t, Key, SValue>* s_arr,
+    unsigned int r_size, unsigned int s_size,
+    unsigned int r_idx, unsigned int s_idx)
+    : _r_arr(r_arr), _s_arr(s_arr),
+    _r_size(r_size), _s_size(s_size),
+    _r_idx(r_idx), _s_idx(s_idx) {
+      while (true) {
+        if (_r_idx == _r_size) {
+          _s_idx = _s_size;
+          break;
+        }
+        if (_s_idx == _s_size) {
+          _r_idx = _r_size;
+          break;
+        }
+        if (std::get<0>(_r_arr[_r_idx]) <
+            std::get<0>(_s_arr[_s_idx])) {
+          _r_idx++;
+          continue;
+        }
+        if (std::get<0>(_s_arr[_s_idx]) <
+            std::get<0>(_r_arr[_r_idx])) {
+          _s_idx++;
+          continue;
+        }
+        if (std::get<1>(_r_arr[_r_idx]) ==
+            std::get<1>(_s_arr[_s_idx])) {
+          break;
+        }
+        if (std::get<1>(_r_arr[_r_idx]) <
+            std::get<1>(_s_arr[_s_idx])) {
+          _r_idx++;
+          continue;
+        }
+        _s_idx++;
+      }
+    }
+    iterator& operator++() {
+      if (_r_idx+1 == _r_size) {
+        _r_idx = _r_size;
+        _s_idx = _s_size;
+        return *this;
+      }
+      if (_s_idx+1 == _s_size) {
+        _r_idx = _r_size;
+        _s_idx = _s_size;
+        return *this;
+      }
+      if (std::get<0>(_r_arr[_r_idx]) ==
+          std::get<0>(_r_arr[_r_idx+1])) {
+        _r_idx++;
+        goto find_match;
+      }
+      if (std::get<0>(_s_arr[_s_idx]) ==
+          std::get<0>(_s_arr[_s_idx+1])) {
+        _s_idx++;
+        goto find_match;
+      }
+      _r_idx++;
+      _s_idx++;
+
+    find_match:
+      while (true) {
+        if (_r_idx == _r_size) {
+          _s_idx = _s_size;
+          break;
+        }
+        if (_s_idx == _s_size) {
+          _r_idx = _r_size;
+          break;
+        }
+        if (std::get<0>(_r_arr[_r_idx]) <
+            std::get<0>(_s_arr[_s_idx])) {
+          _r_idx++;
+          continue;
+        }
+        if (std::get<0>(_s_arr[_s_idx]) <
+            std::get<0>(_r_arr[_r_idx])) {
+          _s_idx++;
+          continue;
+        }
+        if (std::get<1>(_r_arr[_r_idx]) ==
+            std::get<1>(_s_arr[_s_idx])) {
+          break;
+        }
+        if (std::get<1>(_r_arr[_r_idx]) <
+            std::get<1>(_s_arr[_s_idx])) {
+          _r_idx++;
+          continue;
+        }
+        _s_idx++;
+      }
+      return *this;
+    }
+    iterator operator++(int) {
+      iterator retval = *this;
+      ++(*this);
+      return retval;
+    }
+    bool operator==(iterator other) const {
+      return _r_idx == other._r_idx &&
+      _s_idx == other._s_idx &&
+      _r_arr == other._r_arr &&
+      _s_arr == other._s_arr;
+    }
+    bool operator!=(iterator other) const {
+      return !(*this == other);
+    }
+    std::tuple<Key*, RValue*, SValue*>& operator*() {
+      tmp_val = std::make_tuple(&std::get<1>(_r_arr[_r_idx]),
+                                &std::get<2>(_r_arr[_r_idx]),
+                                &std::get<2>(_s_arr[_s_idx]));
+      return tmp_val;
+    }
+  protected:
+    std::tuple<std::size_t, Key, RValue>* _r_arr;
+    std::tuple<std::size_t, Key, SValue>* _s_arr;
+    
+    unsigned int _r_size;
+    unsigned int _s_size;
+    unsigned int _r_idx;
+    unsigned int _s_idx;
+    std::tuple<Key*, RValue*, SValue*> tmp_val;
+  };
+
+ public:
+  iterator begin() {
+    return iterator(_r_sorted, _s_sorted, _r_size, _s_size, 0, 0);
+  }
+
+  iterator end() {
+    return iterator(_r_sorted, _s_sorted, _r_size, _s_size,
+                    _r_size, _s_size);
+  }
+ protected:
+  std::tuple<std::size_t, Key, RValue>* _r_sorted;
+  std::tuple<std::size_t, Key, SValue>* _s_sorted;
+  unsigned int _r_size;
+  unsigned int _s_size;
+};
+
 #endif
--- hashjoin_bench.cc
+++ hashjoin_bench.cc
@@ -51,35 +51,59 @@ static void BM_HMJ_breakdown(benchmark::State& state) {
   unsigned int cores = std::thread::hardware_concurrency();
   auto r = ::create_strvec(size);
   auto s = ::create_strvec(size);
+    //std::vector<std::tuple<std::size_t, std::string, uint64_t>>r_sorted(size);
+    //std::vector<std::tuple<std::size_t, std::string, uint64_t>>s_sorted(size);
   //std::vector<std::tuple<std::size_t, std::string, uint64_t>>s_sorted;
   for (auto _ : state) {
-  //benchmark::DoNotOptimize(std::get<0>(r_sorted[0]) = 1);
-    // this is faster, yet triggers seg fault....
-    // r_sorted.reserve(size);
-    // s_sorted.reserve(size);
-    std::vector<std::tuple<std::size_t, std::string, uint64_t>>r_sorted(size);
-    std::vector<std::tuple<std::size_t, std::string, uint64_t>>s_sorted(size);
+    auto r_sorted = new std::tuple<std::size_t, std::string, uint64_t>[size];
+    auto s_sorted = new std::tuple<std::size_t, std::string, uint64_t>[size];
     ::radix_hash_bf6<std::string, uint64_t>
-        (r.begin(), r.end(), r_sorted.begin(), 11, 0, cores);
+        (r.begin(), r.end(), r_sorted, 11, 0, cores);
     ::radix_hash_bf6<std::string, uint64_t>
-        (s.begin(), s.end(), s_sorted.begin(), 11, 0, cores);
+        (s.begin(), s.end(), s_sorted, 11, 0, cores);
 
-    auto r_iter = r_sorted.begin();
-    auto s_iter = s_sorted.begin();
     uint64_t sum = 0;
     for (int i = 0; i < size; i++) {
-      benchmark::DoNotOptimize(sum+=std::get<2>(*(r_iter++)));
-      benchmark::DoNotOptimize(sum+=std::get<2>(*(s_iter++)));
+      benchmark::DoNotOptimize(sum+=std::get<2>(r_sorted[i]));
+      benchmark::DoNotOptimize(sum+=std::get<2>(s_sorted[i]));
+    }
+    delete[] r_sorted;
+    delete[] s_sorted;
+  }
+  state.SetComplexityN(state.range(0)*2);
+}
+
+static void BM_HashMergeJoin2(benchmark::State& state) {
+  int size = state.range(0);
+  uint64_t sum = 0;
+  auto r = ::create_strvec(size);
+  auto s = ::create_strvec(size);
+  int counter;
+  for (auto _ : state) {
+    auto hmj = HashMergeJoin2<KeyValVec::iterator,
+         KeyValVec::iterator>(r.begin(), r.end(),
+             s.begin(), s.end());
+    sum = 0;
+    //benchmark::DoNotOptimize(hmj.begin());
+    for (auto tuple : hmj) {
+      //benchmark::DoNotOptimize(sum += *std::get<1>(tuple)+*std::get<2>(tuple));
+      benchmark::DoNotOptimize(sum++);
     }
   }
   state.SetComplexityN(state.range(0)*2);
 }
 
-BENCHMARK(BM_hash_join_raw)->Range(1<<18, 1<<23)->Complexity(benchmark::oN)
+BENCHMARK(BM_hash_join_raw)->RangeMultiplier(2)
+->Range(1<<18, 1<<23)->Complexity(benchmark::oN)
 ->UseRealTime();
-BENCHMARK(BM_HashMergeJoin)->Range(1<<18, 1<<23)->Complexity(benchmark::oN)
+BENCHMARK(BM_HashMergeJoin)->RangeMultiplier(2)
+->Range(1<<18, 1<<23)->Complexity(benchmark::oN)
 ->UseRealTime();
-BENCHMARK(BM_HMJ_breakdown)->Range(1<<18, 1<<23)->Complexity(benchmark::oN)
+BENCHMARK(BM_HMJ_breakdown)->RangeMultiplier(2)
+->Range(1<<18, 1<<23)->Complexity(benchmark::oN)
+->UseRealTime();
+BENCHMARK(BM_HashMergeJoin2)->RangeMultiplier(2)
+->Range(1<<18, 1<<23)->Complexity(benchmark::oN)
 ->UseRealTime();
 
 BENCHMARK_MAIN();
