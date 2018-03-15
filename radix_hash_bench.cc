1482399d1e4561447c889d03e50fb406b2c6860f
Felix Chern <idryman@gmail.com>
2018-03-14 23:42:15 -0400

    cleanup some dead code

--- radix_hash_bench.cc
+++ radix_hash_bench.cc
@@ -274,80 +274,6 @@ static void BM_pdqsort_string(benchmark::State& state) {
   state.counters["Swap"] = u_after.ru_nswap - u_before.ru_nswap;
 }
 
-static void BM_bf6_breakdown1(benchmark::State& state) {
-  int size = state.range(0);
-  std::vector<std::tuple<std::size_t, std::string, uint64_t>> dst(size);
-  auto src = ::create_strvec(size);
-  unsigned int num_threads = std::thread::hardware_concurrency();
-  struct rusage u_before, u_after;
-  getrusage(RUSAGE_SELF, &u_before);
-
-  int partition_bits = 11;
-
-  typedef std::vector<std::pair<std::string, uint64_t>> KeyValVec;
-  typedef std::vector<std::tuple<std::size_t, std::string, uint64_t>>
-    HashKeyValVec;
-
-#ifdef HAVE_PAPI
-  assert(PAPI_start_counters(events, e_num) == PAPI_OK);
-#endif
-
-  for (auto _ : state) {
-    //auto& begin = src.begin();
-    //auto& end = src.end();
-    //auto& dst_iter = dst.begin();
-    int input_num, shift, partitions, thread_partition;
-    std::atomic_uint a_counter(0);
-    ThreadBarrier barrier(num_threads);
-
-    partitions = 1 << partition_bits;
-    input_num = size;
-    thread_partition = input_num / num_threads;
-
-    shift = 64 - partition_bits;
-
-    std::vector<unsigned int> shared_counters(partitions*num_threads);
-    std::vector<std::pair<unsigned int, unsigned int>> indexes(partitions);
-    std::vector<std::thread> threads(num_threads);
-
-    for (unsigned int i = 0; i < num_threads-1; i++) {
-      threads[i] = std::thread(radix_hash_bf6_worker<std::string,uint64_t,
-                               std::hash<std::string>,
-                               KeyValVec::iterator, HashKeyValVec::iterator>,
-                               src.begin() + i * thread_partition,
-                               src.begin() + (i+1) * thread_partition,
-                               dst.begin(), i, num_threads,
-                               &barrier, &shared_counters, &indexes,
-                               partitions, shift);
-    }
-
-    radix_hash_bf6_worker<std::string,
-                          uint64_t,
-                          std::hash<std::string>>
-      (src.begin()+(num_threads-1)*thread_partition,
-       src.end(), dst.begin(), num_threads-1, num_threads,
-       &barrier, &shared_counters, &indexes,
-       partitions, shift);
-    for (unsigned int i = 0; i < num_threads-1; i++) {
-      threads[i].join();
-    }
-  }
-
-#ifdef HAVE_PAPI
-  assert(PAPI_stop_counters(papi_values, e_num) == PAPI_OK);
-  state.counters["L1 miss"] = papi_values[0];
-  state.counters["L2 miss"] = papi_values[1];
-  state.counters["L3 miss"] = papi_values[2];
-#endif
-
-  getrusage(RUSAGE_SELF, &u_after);
-
-  state.SetComplexityN(state.range(0));
-  state.counters["Minor"] = u_after.ru_minflt - u_before.ru_minflt;
-  state.counters["Major"] = u_after.ru_majflt - u_before.ru_majflt;
-  state.counters["Swap"] = u_after.ru_nswap - u_before.ru_nswap;
-}
-
 static void RadixArguments(benchmark::internal::Benchmark* b) {
   for (int i = 11; i < 12; i+=1) {
     for (int j = (1 << 18); j <= (1 << 23); j <<= 1) {
@@ -481,9 +407,6 @@ BENCHMARK(BM_radix_hash_bf4_str)->Apply(RadixArguments)
 BENCHMARK(BM_radix_hash_bf5_str)->Apply(RadixArguments)
 ->Complexity(benchmark::oN)->UseRealTime();
 BENCHMARK(BM_radix_hash_bf6_str)->Apply(RadixArguments)
-->Complexity(benchmark::oN)->UseRealTime();
-
-BENCHMARK(BM_bf6_breakdown1)->Apply(RadixArguments)
 ->Complexity(benchmark::oN)->UseRealTime();
 
 BENCHMARK(BM_bf6_1_thread)->Apply(RadixArguments)
