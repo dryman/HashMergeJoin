9f7dfd3f0042e27c63adee771a7e302bebc70498
dryman <idryman@gmail.com>
2018-03-16 07:59:22 -0700

    rs2 - short circuit use std::sort instead of insertion sort

--- radix_sort.h
+++ radix_sort.h
@@ -402,4 +402,238 @@ template <typename Key,
   }
 }
 
+template <typename Key, typename Value>
+  bool rs2_pair_cmp (std::pair<Key, Value> a,
+                     std::pair<Key, Value> b) {
+  return std::get<0>(a) < std::get<0>(b);
+}
+
+template <typename Key,
+  typename Value,
+  typename RandomAccessIterator>
+  void rs2_helper_s(RandomAccessIterator dst,
+                    const unsigned int (*super_indexes)[2],
+                    int mask_bits,
+                    int partition_bits) {
+  std::pair<Key, Value> tmp_bucket;
+  Key h, mask;
+  unsigned int partitions, shift,
+    idx_i, idx_j, idx_c, s_begin, s_end, iter;
+  int new_mask_bits;
+
+  partitions = 1 << partition_bits;
+  mask = (1ULL << mask_bits);
+  mask -= 1;
+  shift = mask_bits < partition_bits ? 0 : mask_bits - partition_bits;
+
+  unsigned int counters[partitions];
+  unsigned int indexes[partitions][2];
+
+  for (unsigned int s = 0; s < partitions; s++) {
+    s_begin = super_indexes[s][0];
+    s_end = super_indexes[s][1];
+    if (s_end - s_begin < 2)
+      continue;
+    // Partition too small, use insertion sort instead.
+    if (s_end - s_begin < partitions) {
+      std::sort(dst + s_begin, dst + s_end, rs2_pair_cmp<Key, Value>);
+      continue;
+    }
+    // Setup counters for counting sort.
+    for (unsigned int i = 0; i < partitions; i++)
+      counters[i] = 0;
+    indexes[0][0] = s_begin;
+    for (unsigned int i = s_begin; i < s_end; i++) {
+      h = std::get<0>(dst[i]);
+      counters[(h & mask) >> shift]++;
+    }
+    for (unsigned int i = 0; i < partitions - 1; i++) {
+      indexes[i][1] = indexes[i+1][0] = indexes[i][0] + counters[i];
+    }
+    indexes[partitions-1][1] = indexes[partitions-1][0]
+      + counters[partitions-1];
+
+    new_mask_bits = mask_bits - partition_bits;
+    iter = 0;
+    while (iter < partitions) {
+      idx_i = indexes[iter][0];
+      if (idx_i >= indexes[iter][1]) {
+        iter++;
+        continue;
+      }
+      h = std::get<0>(dst[idx_i]);
+      idx_c = (h & mask) >> shift;
+      if (idx_c == iter) {
+        indexes[iter][0]++;
+        continue;
+      }
+      tmp_bucket = std::move(dst[idx_i]);
+      do {
+        h = std::get<0>(tmp_bucket);
+        idx_c = (h & mask) >> shift;
+        idx_j = indexes[idx_c][0]++;
+        std::swap(dst[idx_j], tmp_bucket);
+      } while (idx_j > idx_i);
+    }
+
+    if (new_mask_bits <= 0) {
+      continue;
+    }
+
+    // Reset indexes
+    indexes[0][0] = s_begin;
+    for (unsigned int i = 1; i < partitions; i++) {
+      indexes[i][0] = indexes[i-1][1];
+    }
+    rs2_helper_s<Key,Value,RandomAccessIterator>
+      (dst, indexes, new_mask_bits, partition_bits);
+  }
+}
+
+template <typename Key,
+  typename Value,
+  typename RandomAccessIterator>
+  void rs2_helper_p(RandomAccessIterator dst,
+                    const std::vector<std::pair<unsigned int,
+                    unsigned int>>& super_indexes,
+                    int mask_bits,
+                    int partition_bits,
+                    std::atomic_uint* super_counter) {
+  std::pair<Key, Value> tmp_bucket;
+  Key h, mask;
+  unsigned int s_idx, partitions, shift,
+    idx_i, idx_j, idx_c, s_begin, s_end, iter;
+  int new_mask_bits;
+
+  partitions = 1 << partition_bits;
+  mask = (1ULL << mask_bits);
+  mask -= 1;
+  shift = mask_bits < partition_bits ? 0 : mask_bits - partition_bits;
+
+  unsigned int counters[partitions];
+  unsigned int indexes[partitions][2];
+
+  s_idx = super_counter->fetch_add(1, std::memory_order_relaxed);
+
+  // TODO change s to a atomic variable and use a boolean to determine
+  // to use it or not.
+  while (s_idx < partitions) {
+    s_begin = super_indexes[s_idx].first;
+    s_end = super_indexes[s_idx].second;
+    if (s_end - s_begin < 2) {
+      s_idx = super_counter->fetch_add(1, std::memory_order_relaxed);
+      continue;
+    }
+    // Partition too small, use insertion sort instead.
+    if (s_end - s_begin < partitions) {
+      std::sort(dst + s_begin, dst + s_end, rs2_pair_cmp<Key, Value>);
+      s_idx = super_counter->fetch_add(1, std::memory_order_relaxed);
+      continue;
+    }
+    // Setup counters for counting sort.
+    for (unsigned int i = 0; i < partitions; i++)
+      counters[i] = 0;
+    indexes[0][0] = s_begin;
+    for (unsigned int i = s_begin; i < s_end; i++) {
+      h = std::get<0>(dst[i]);
+      counters[(h & mask) >> shift]++;
+    }
+    for (unsigned int i = 0; i < partitions - 1; i++) {
+      indexes[i][1] = indexes[i+1][0] = indexes[i][0] + counters[i];
+    }
+    indexes[partitions-1][1] = indexes[partitions-1][0]
+      + counters[partitions-1];
+
+    new_mask_bits = mask_bits - partition_bits;
+    iter = 0;
+
+    while (iter < partitions) {
+      idx_i = indexes[iter][0];
+      if (idx_i >= indexes[iter][1]) {
+        iter++;
+        continue;
+      }
+      h = std::get<0>(dst[idx_i]);
+      idx_c = (h & mask) >> shift;
+      if (idx_c == iter) {
+        indexes[iter][0]++;
+        continue;
+      }
+      tmp_bucket = std::move(dst[idx_i]);
+      do {
+        h = std::get<0>(tmp_bucket);
+        idx_c = (h & mask) >> shift;
+        idx_j = indexes[idx_c][0]++;
+        std::swap(dst[idx_j], tmp_bucket);
+      } while (idx_j > idx_i);
+    }
+
+    if (new_mask_bits <= 0) {
+      s_idx = super_counter->fetch_add(1, std::memory_order_relaxed);
+      continue;
+    }
+
+    // Reset indexes
+    indexes[0][0] = s_begin;
+    for (unsigned int i = 1; i < partitions; i++) {
+      indexes[i][0] = indexes[i-1][1];
+    }
+    rs2_helper_s<Key,Value,RandomAccessIterator>
+      (dst, indexes, new_mask_bits, partition_bits);
+    s_idx = super_counter->fetch_add(1, std::memory_order_relaxed);
+  }
+}
+
+template <typename Key,
+  typename Value,
+  typename RandomAccessIterator>
+  void radix_sort_2(RandomAccessIterator dst,
+                    unsigned int input_num,
+                    int partition_bits,
+                    int num_threads) {
+  static_assert(std::is_unsigned<Key>::value, "Key must be an unsigned arithmic type.");
+  int shift, partitions, thread_partition, new_mask_bits;
+  std::atomic_uint a_counter(0);
+  ThreadBarrier barrier(num_threads);
+
+  partitions = 1 << partition_bits;
+  thread_partition = input_num / num_threads;
+
+  shift = sizeof(Key)*8 - partition_bits;
+
+  std::vector<std::atomic_ullong> shared_counters(partitions);
+  std::vector<std::pair<unsigned int, unsigned int>> indexes(partitions);
+  std::vector<std::thread> threads(num_threads);
+
+  for (int i = 0; i < num_threads-1; i++) {
+    threads[i] = std::thread(rs1_worker<Key,Value,
+                             RandomAccessIterator>,
+                             dst, i * thread_partition,
+                             (i+1) * thread_partition,
+                             i, &barrier, &shared_counters, &indexes,
+                             partitions, shift);
+  }
+
+  rs1_worker<Key,Value>(dst, (num_threads-1)*thread_partition,
+                        input_num, num_threads-1, &barrier,
+                        &shared_counters, &indexes,
+                        partitions, shift);
+  for (int i = 0; i < num_threads-1; i++) {
+    threads[i].join();
+  }
+
+  new_mask_bits = sizeof(Key)*8 - partition_bits;
+
+  for (int i = 0; i < num_threads-1; i++) {
+    threads[i] = std::thread(rs2_helper_p<Key,Value, RandomAccessIterator>,
+                             dst, indexes, new_mask_bits,
+                             partition_bits, &a_counter);
+  }
+  rs2_helper_p<Key,Value, RandomAccessIterator>(
+      dst, indexes, new_mask_bits,
+      partition_bits, &a_counter);
+  for (int i = 0; i < num_threads-1; i++) {
+    threads[i].join();
+  }
+}
 #endif
--- radix_sort_bench.cc
+++ radix_sort_bench.cc
@@ -239,6 +239,44 @@ static void BM_radix_sort_1_multi(benchmark::State& state) {
     / state.iterations();
 }
 
+static void BM_radix_sort_2_multi(benchmark::State& state) {
+  int size = state.range(0);
+  std::default_random_engine generator;
+  std::uniform_int_distribution<std::size_t> distribution;
+  std::vector<std::pair<std::size_t, uint64_t>> input;
+  std::vector<std::pair<std::size_t, uint64_t>> work;
+  unsigned int cores = std::thread::hardware_concurrency();
+
+  for (int i = 0; i < size; i++) {
+    std::size_t r = distribution(generator);
+    input.push_back(std::make_pair(r, i));
+  }
+
+  struct rusage u_before, u_after;
+  getrusage(RUSAGE_SELF, &u_before);
+
+  RESET_ACC_COUNTERS;
+  for (auto _ : state) {
+    state.PauseTiming();
+    work = input;
+    state.ResumeTiming();
+    START_COUNTERS;
+    ::radix_sort_2<std::size_t, uint64_t>(work.begin(), size,
+                                          state.range(1), cores);
+    ACCUMULATE_COUNTERS;
+  }
+  REPORT_COUNTERS(state);
+
+  getrusage(RUSAGE_SELF, &u_after);
+  state.SetComplexityN(state.range(0));
+  state.counters["Minor"] = (u_after.ru_minflt - u_before.ru_minflt)
+    / state.iterations();
+  state.counters["Major"] = (u_after.ru_majflt - u_before.ru_majflt)
+    / state.iterations();
+  state.counters["Swap"] = (u_after.ru_nswap - u_before.ru_nswap)
+    / state.iterations();
+}
+
 BENCHMARK(BM_std_sort_int)->RangeMultiplier(2)->Range(1<<18, 1<<23)
 ->Complexity(benchmark::oN)->UseRealTime();
 BENCHMARK(BM_tbb_int)->RangeMultiplier(2)->Range(1<<18, 1<<23)
@@ -246,6 +284,8 @@ BENCHMARK(BM_tbb_int)->RangeMultiplier(2)->Range(1<<18, 1<<23)
 BENCHMARK(BM_pdqsort_int)->RangeMultiplier(2)->Range(1<<18, 1<<23)
 ->Complexity(benchmark::oN)->UseRealTime();
 
+/********************  radix sort 1 single ***********************/
+
 BENCHMARK(BM_radix_sort_1_single)
 ->Args({1<<18, 3})
 ->Args({1<<19, 3})
@@ -288,6 +328,9 @@ BENCHMARK(BM_radix_sort_1_single)
 ->Args({1<<22, 13})
 ->Complexity(benchmark::oN)->UseRealTime();
 
+/********************  radix sort 1 multi ***********************/
+
+
 BENCHMARK(BM_radix_sort_1_multi)
 ->Args({1<<18, 3})
 ->Args({1<<19, 3})
@@ -328,6 +371,17 @@ BENCHMARK(BM_radix_sort_1_multi)
 ->Args({1<<20, 13})
 ->Args({1<<21, 13})
 ->Args({1<<22, 13})
+->Complexity(benchmark::oN)->UseRealTime();
+
+/********************  radix sort 2 ***********************/
+
+BENCHMARK(BM_radix_sort_2_multi)
+->Args({1<<18, 11})
+->Args({1<<19, 11})
+->Args({1<<20, 11})
+->Args({1<<21, 11})
+->Args({1<<22, 11})
+->Args({1<<23, 11})
 ->Complexity(benchmark::oN)->UseRealTime();
 
 BENCHMARK_MAIN();
